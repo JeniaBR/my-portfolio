3c10a93112af8bdc7a9f6c865873be71c2b5d2d2
Jenia Brook <jeniabrook@gmail.com>
2017-08-07 20:30:36 +0300

    Styling Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,14 @@ class Navbar extends React.Component {
   render(){
     return(
       <nav className="navbar-container">
-        <div className="my-name">Jenia Brook</div>
+        <div className="mobile-wrapper">
+          <div className="my-name">Jenia Brook</div>
+          <div className="hamburger-menu">
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+          </div>
+        </div>
         <div className="nav-links">
           <a href="#">porfolio</a>
           <a href="#">about</a>
