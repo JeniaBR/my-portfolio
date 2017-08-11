bd09f9ac5f30bbf190d38290fcc1d534c73b1558
Jenia Brook <jeniabrook@gmail.com>
2017-08-11 17:08:32 +0300

    Css fixes

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -6,14 +6,14 @@ const AboutMe = () => {
   return (
     <div className="about-me-container">
       <div className="wrapper">
-        <h2 className="first">Hi I'm Jenia Brook,</h2>
-        <h2 className="second">a fullstack engineer</h2>
-        <h2 className="third">who loves JavaScript</h2>
+        <div className="header">
+          <h2>Hi I'm Jenia Brook, a fullstack engineer who loves JavaScript</h2>
+        </div>
+        <img className="img" src={myImage} alt="Me :)"/>
+        <p className="paragraph">
+          I like to focus on client-side development for all devices using web technologies (JavaScript, CSS and HTML) with big attention to adaptive/responsive web design, to serve accessible content for each devices.
+        </p>
       </div>
-      <img className="img" src={myImage} alt="Me :)"/>
-      <p className="wrapper">
-        I like to focus on client-side development for all devices using web technologies (JavaScript, CSS and HTML) with big attention to adaptive/responsive web design, to serve accessible content for each devices.
-      </p>
     </div>
   );
 }
