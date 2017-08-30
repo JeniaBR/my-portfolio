94b51dc4abe89b795e46ce673d62b57cbd0478ad
Jenia Brook <jeniabrook@gmail.com>
2017-08-30 20:08:24 +0300

    uploading my projects screenshots

--- src/make-portfolio.js
+++ src/make-portfolio.js
@@ -1,6 +1,6 @@
-import cabin from './img/portfolio/cabin.png';
-import cake from './img/portfolio/cake.png';
-import circus from './img/portfolio/circus.png';
+import catchOfTheDay from './img/portfolio/catch.png';
+import emoji from './img/portfolio/emoji.png';
+import battle from './img/portfolio/battle.png';
 import game from './img/portfolio/game.png';
 import safe from './img/portfolio/safe.png';
 import submarine from './img/portfolio/submarine.png';
@@ -8,22 +8,22 @@ import submarine from './img/portfolio/submarine.png';
 const portfolio = [
   {
     id: 0,
-    title: "Cabin",
-    img: cabin,
+    title: "Catch of the Day",
+    img: catchOfTheDay,
     description: 'Lorem ipsum dolor sit amet, id mei liber dicit fabulas. Inimicus molestiae similique pri ne, ei ceteros oportere est, oblique nostrum ne nec. Ea partiendo consequat ius. At est fabulas quaestio.',
     technologies: ['React', 'Redux', 'Webpack']
   },
   {
     id: 1,
-    title: "Cake",
-    img: cake,
+    title: "Emoji Search App",
+    img: emoji,
     description: 'Lorem ipsum dolor sit amet, id mei liber dicit fabulas. Inimicus molestiae similique pri ne, ei ceteros oportere est, oblique nostrum ne nec. Ea partiendo consequat ius. At est fabulas quaestio.',
     technologies: ['React', 'Redux', 'Webpack']
   },
   {
     id: 2,
-    title: "Circus",
-    img: circus,
+    title: "Github Battle",
+    img: battle,
     description: 'Lorem ipsum dolor sit amet, id mei liber dicit fabulas. Inimicus molestiae similique pri ne, ei ceteros oportere est, oblique nostrum ne nec. Ea partiendo consequat ius. At est fabulas quaestio.',
     technologies: ['React', 'Redux', 'Webpack']
   },
