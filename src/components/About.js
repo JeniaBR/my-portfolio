a46674a1d385b1ba599e429ff5f804b8d125d876
Jenia Brook <jeniabrook@gmail.com>
2017-08-31 19:54:35 +0300

    css fixes for mobile layout

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import './About.css';
 
 const About = (props) => {
   return(
-    <section className="container">
+    <section className="about-container">
       <h2>About</h2>
       <p>I am an aspiring Front-End Web Developer. I graduated from the State University of New York (SUNY) College at Oswego with a B.A. in Global & International Studies; Concentration in Global Economy. I also double minored in Business Administration and Economics. I love challenging ideas and thinking outside of the box in order to gain an unclouded perspective, which my major heavily emphasized. Since graduating, I have experienced a lot of new places and people, allowing me to gain a better understanding of myself. Coding appeared in my life through the indirect influences of my friends, mixed with my growing interest on the subject. Web developing is engaging, challenging, and rewarding, which is how it drew me in. Now, I am progressing through the cirriculum of Free Code Camp's Front-End Development Certification.</p>
     </section>
