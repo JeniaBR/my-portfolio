c11e5404aa007404c90750a0dd8d65661b73e2b2
Jenia Brook <jeniabrook@gmail.com>
2017-08-31 16:43:40 +0300

    deleted dummy porfolio added real img portfolio

--- src/make-portfolio.js
+++ src/make-portfolio.js
@@ -1,53 +1,61 @@
 import catchOfTheDay from './img/portfolio/catch.png';
 import emoji from './img/portfolio/emoji.png';
 import battle from './img/portfolio/battle.png';
-import game from './img/portfolio/game.png';
-import safe from './img/portfolio/safe.png';
-import submarine from './img/portfolio/submarine.png';
+import delicious from './img/portfolio/delicious.png';
+import reduxstagram from './img/portfolio/reduxstagram.png';
+import simon from './img/portfolio/simon.png';
+import tic from './img/portfolio/tic-tac-toe.png';
 
 const portfolio = [
   {
     id: 0,
+    title: "Delicious Web App",
+    img: delicious,
+    description: 'Lorem ipsum dolor sit amet, id mei liber dicit fabulas. Inimicus molestiae similique pri ne, ei ceteros oportere est, oblique nostrum ne nec. Ea partiendo consequat ius. At est fabulas quaestio.',
+    technologies: ['React', 'Redux', 'Webpack']
+  },
+  {
+    id: 1,
     title: "Catch of the Day",
     img: catchOfTheDay,
     description: 'Lorem ipsum dolor sit amet, id mei liber dicit fabulas. Inimicus molestiae similique pri ne, ei ceteros oportere est, oblique nostrum ne nec. Ea partiendo consequat ius. At est fabulas quaestio.',
     technologies: ['React', 'Redux', 'Webpack']
   },
   {
-    id: 1,
-    title: "Emoji Search App",
-    img: emoji,
+    id: 2,
+    title: "Reduxstagram",
+    img: reduxstagram,
     description: 'Lorem ipsum dolor sit amet, id mei liber dicit fabulas. Inimicus molestiae similique pri ne, ei ceteros oportere est, oblique nostrum ne nec. Ea partiendo consequat ius. At est fabulas quaestio.',
     technologies: ['React', 'Redux', 'Webpack']
   },
   {
-    id: 2,
+    id: 3,
     title: "Github Battle",
     img: battle,
     description: 'Lorem ipsum dolor sit amet, id mei liber dicit fabulas. Inimicus molestiae similique pri ne, ei ceteros oportere est, oblique nostrum ne nec. Ea partiendo consequat ius. At est fabulas quaestio.',
     technologies: ['React', 'Redux', 'Webpack']
   },
   {
-    id: 3,
-    title: "Game",
-    img: game,
-    description: 'Lorem ipsum dolor sit amet, id mei liber dicit fabulas. Inimicus molestiae similique pri ne, ei ceteros oportere est, oblique nostrum ne nec. Ea partiendo consequat ius. At est fabulas quaestio.',
-    technologies: ['React', 'Redux', 'Webpack']
-  },
-  {
     id: 4,
-    title: "Safe",
-    img: safe,
+    title: "Tic Tac Toe",
+    img: tic,
     description: 'Lorem ipsum dolor sit amet, id mei liber dicit fabulas. Inimicus molestiae similique pri ne, ei ceteros oportere est, oblique nostrum ne nec. Ea partiendo consequat ius. At est fabulas quaestio.',
     technologies: ['React', 'Redux', 'Webpack']
   },
   {
     id: 5,
-    title: "Submarine",
-    img: submarine,
+    title: "Emoji Search App",
+    img: emoji,
     description: 'Lorem ipsum dolor sit amet, id mei liber dicit fabulas. Inimicus molestiae similique pri ne, ei ceteros oportere est, oblique nostrum ne nec. Ea partiendo consequat ius. At est fabulas quaestio.',
     technologies: ['React', 'Redux', 'Webpack']
   },
+  {
+    id: 6,
+    title: "Simon Game",
+    img: simon,
+    description: 'Lorem ipsum dolor sit amet, id mei liber dicit fabulas. Inimicus molestiae similique pri ne, ei ceteros oportere est, oblique nostrum ne nec. Ea partiendo consequat ius. At est fabulas quaestio.',
+    technologies: ['React', 'Redux', 'Webpack']
+  }
 ];
 
 export default portfolio;
